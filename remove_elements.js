abc6b37886b8393ca547247ba28f75937fe3be37
Sean Cronin <seancron@gmail.com>
2011-09-24 23:31:35 -0400

    Now uses the jQuery on the site to remove elements

--- remove_elements.js
+++ remove_elements.js
@@ -1,22 +1,7 @@
-var toDelete = [    "fastfacts",
-                    "fastFacts" 
+var toDelete = [    "#studentLife",
+                    "#fastfacts,p",
                ];
 
-function removeElements(parentDiv, childDiv) {
-    if (childDiv == parentDiv) {
-        //alert("The parent div cannot be removed.");
-    }
-    else if (document.getElementById(childDiv)) {
-        var child = document.getElementById(childDiv);
-        var parent = document.getElementById(parentDiv);
-        parent.removeChild(child);
-    }
-    else {
-        //alert("Child div has already been removed or does not exist.");
-        //return false;
-    }
-}
-
-for (element in toDelete) {
-    document.getElementById(element).parentNode.removeChild(element);
+for (selector in toDelete) {
+    $(selector).remove();
 }    
